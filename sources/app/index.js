fc8f772718935b498dd86ecd4b1ee27dd4c3c76b
nsmirnova <smirnovanastya1602@gmail.com>
2016-03-20 21:41:55 +0200

    Create main application module.
    Added dependencies
    Saved user role types in constant,
    Describe auth route in config

--- sources/app/index.js
+++ sources/app/index.js
@@ -2,7 +2,34 @@
 
   'use strict';
 
-  var app = angular.module('games', ['firebase']);
+  angular
+    .module('games', [
+      'ui.router',
+      'firebase',
+      'games.auth'
+    ])
+    .constant('Settings', {
+    	roles: {
+    		1: {
+    			name: 'superadmin',
+    			title: 'Super Admin'
+    		},
+    		2: {
+    			name: 'user',
+    			title: 'User'
+    		}
+    	}
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/auth');
 
-  console.log('hello world!');
+      $stateProvider
+        .state('auth', {
+          url: '/auth',
+          templateUrl: 'sources/app/auth/auth.html',
+          controller: 'AuthCtrl',
+          controllerAs: 'auth'
+        });
+    });
+
 })();
